7b6f7925eb07fb613b79fbe802427dd2d3c0ce0c
soundaryakm <45666492+soundaryakm@users.noreply.github.com>
2024-03-15 19:00:38 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {facebook} from "@expo/vector-icons"
+
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    if (email === 'example@example.com') {
+      console.log('Login successful');
+      navigation.navigate('BMI');
+    } 
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/Bmi.jpeg')} style={styles.icon2} />
+      <Text style={styles.title}>Login to your account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Login" onPress={() => navigation.navigate('BMI')} />
+      <Text style={styles.title2}>-- Or sign in with --</Text>
+      <View style={styles.logo}>
+        <Image source={require('../assets/face.png')} style={styles.icon} />
+        <Image source={require('../assets/google.jpeg')} style={styles.icon} />
+        <Image source={require('../assets/twitter.png')} style={styles.icon} />
+      </View>
+     
+      <TouchableOpacity onPress={() => navigation.navigate('Registration')}>
+        <Text style={styles.signupText}>Don't have an account? <Text style={styles.signupLink}>Sign up</Text></Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    flexDirection: "row",
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: 20
+  },
+  input: {
+    width: '100%',
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  title2: {
+    marginTop: 30,
+  },
+  buttonText: {
+    color: '#fff',
+    marginLeft: 10,
+  },
+  icon: {
+    marginTop: 20,
+    width: 40,
+    height: 30,
+  },
+  icon2: {
+    marginTop: -20,
+    width: 250,
+    height: 250,
+  },
+  signupText: {
+    marginTop: 30,
+    fontSize: 16,
+    color: '#333',
+  },
+  signupLink: {
+    textDecorationLine: 'underline',
+    color: 'blue',
+  },
+});
+
+export default Login;
